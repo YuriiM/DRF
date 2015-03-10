31bd84d2ef0f243067e3b827bb46229fe44a740e
Yurii <yuriimys@gmail.com>
2015-03-10 14:31:54 +0200

    Serialization

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -11,5 +11,6 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
-                               namespace='rest_framework'))
+                               namespace='rest_framework')),
+    url(r'^', include('snippets.urls')),
 ]
